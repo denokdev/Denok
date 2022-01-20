1f98e076372ed5dfaec1b813694d23f29dde00ac
Wuriyanto <wuriyanto48@yahoo.co.id>
2022-01-20 19:01:57 +0700

    base response: add error handling

--- Denok.Web/Base/Model/BaseResponseModel.cs
+++ Denok.Web/Base/Model/BaseResponseModel.cs
@@ -19,25 +19,25 @@ namespace Denok.Web.Base.Model
         public string CreatedBy { get; set; }
 
         [JsonPropertyName("createdAt")]
-        public DateTime CreatedAt 
+        public DateTime? CreatedAt 
         { 
             get => Utils.Utils.ConvertDateTimeToLocalTimeZone(_createdAt); 
-            set => _createdAt = value; 
+            set => _createdAt = value.Value; 
         }
 
         [JsonPropertyName("updatedAt")]
-        public DateTime UpdatedAt 
+        public DateTime? UpdatedAt 
         { 
             get => Utils.Utils.ConvertDateTimeToLocalTimeZone(_updatedAt); 
-            set => _updatedAt = value; 
+            set => _updatedAt = value.Value; 
         }
         
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [JsonPropertyName("deletedAt")]
-        public DateTime DeletedAt 
+        public DateTime? DeletedAt 
         {
             get => Utils.Utils.ConvertDateTimeToLocalTimeZone(_deletedAt);
-            set => _deletedAt = value; 
+            set => _deletedAt = value.Value; 
         }
 
         [JsonPropertyName("isDeleted")]
