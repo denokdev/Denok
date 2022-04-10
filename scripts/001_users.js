4a8462513835c1a73981cd5d9f9a07d5d0384c42
Wuriyanto <wuriyanto48@yahoo.co.id>
2022-04-10 11:49:23 +0700

    update mongo scripts

--- scripts/001_users.js
+++ scripts/001_users.js
@@ -17,17 +17,17 @@ db.createCollection('users');
 
 db.users.insertMany([
     { 
-        "_id" : ObjectId("61b9f08d4010cb9634dd4124"), 
+        "_id" : ObjectId("61c02ceca87af56b7b681725"), 
         "createdBy" : null, 
-        "createdAt" : ISODate("2021-12-15T13:41:33.355Z"), 
-        "updatedAt" : ISODate("2021-12-15T13:41:33.362Z"), 
+        "createdAt" : ISODate("2021-12-20T07:12:43.977Z"), 
+        "updatedAt" : ISODate("2021-12-20T07:12:43.977Z"), 
         "deletedAt" : ISODate("0001-01-01T00:00:00Z"), 
         "isDeleted" : false, 
-        "username" : "admin", 
-        "email" : "telkomdigitalsolution2019@gmail.com", 
+        "username" : "user", 
+        "email" : "user@gmail.com", 
         "profilePicture" : null, 
-        "phoneNumber" : "082262526272", 
-        "password" : "1000:vPPzbuhPP8hPxhesa+9yefrZ9AlGXPZv:oTwCZv9X58brjTD2OnKK2sfrgdM=", 
+        "phoneNumber" : "082111777999", 
+        "password" : "1000:kB8WuPggBE9BbEuBSU+8GlaBhdGhm4/7:KKfl5A37TEkv/wXKehTPVJSw+JE=", 
         "deviceToken" : null, 
         "otpSecret" : null, 
         "status" : "ACTIVE" 
